e1d6c38c874c994efd3bc40faf8d6ae9307c9889
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-02-01 15:33:23 +0700

    handler create

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,6 +1,22 @@
-exports.create = (req, res) => {
-  res.send({ message: "create handler" });
-};
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.utils");
+const ApiError = require("../api-error");
+
+exports.create = async (req, res, next) => {
+  if (!req.body?.name) {
+    return next( new ApiError(400, 'Name can not be empty') );
+  }
+
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.create(req.body);
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, 'An error occured while creating the contact')
+    );
+  }
+}
 
 exports.findAll = (req, res) => {
   res.send({message: 'findAll handler'})
